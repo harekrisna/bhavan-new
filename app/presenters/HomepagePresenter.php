b24569e8b9f17999966402fce34ce3cedd91957d
Bárta Michal - Allrisk <barta.michal@allrisk.cz>
2017-03-20 16:50:57 +0100

    responsive building

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -3,15 +3,42 @@
 namespace App\Presenters;
 
 use Nette;
-use App\Model;
+use Nette\Utils\DateTime;
+use Tracy\Debugger;
 
+class HomepagePresenter extends BasePresenter  {
+	
+	public function renderDefault()	{
+		$this->template->slides = $this->slide->getAll()
+											  ->where('show_from IS NULL OR show_from < NOW()')
+											  ->where('show_to IS NULL OR show_to > NOW()')
+											  ->order('position');
+											  
+		$this->template->now = new DateTime('now');
 
-class HomepagePresenter extends BasePresenter
-{
-
-	public function renderDefault()
-	{
-		$this->template->anyVariable = 'any value';
+		$this->template->news_count = $this->news->getAll()
+												 ->where('show_from IS NULL OR show_from < NOW()')
+												 ->where('show_to IS NULL OR show_to > NOW()')
+												 ->count();
+	
+		$this->template->max_group_news = $this->news->maxGroupNews();
+	
+		$this->template->event_news = $this->news->getAll()
+												 ->where(['news_type.type' => 'event'])
+												 ->where('show_from IS NULL OR show_from < NOW()')
+												 ->where('show_to IS NULL OR show_to > NOW()')
+												 ->order('date_from ASC');
+											  
+		$this->template->photo_news = $this->news->getAll()
+												 ->where(['news_type.type' => 'photo'])
+												 ->where('show_from IS NULL OR show_from < NOW()')
+												 ->where('show_to IS NULL OR show_to > NOW()')
+												 ->order('date_from DESC');
+											  
+		$this->template->audio_news = $this->news->getAll()
+												 ->where(['news_type.type' => 'audio'])
+												 ->where('show_from IS NULL OR show_from < NOW()')
+												 ->where('show_to IS NULL OR show_to > NOW()')
+												 ->order('date_from DESC');
 	}
-
 }
