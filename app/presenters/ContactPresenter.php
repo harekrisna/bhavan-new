ea7278842af409188c70278dfc70fc87ee7cb4fc
Bárta Michal - Allrisk <barta.michal@allrisk.cz>
2017-04-18 16:34:55 +0200

    audio komplet, kontakt

---
+++ app/presenters/ContactPresenter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette,
+	App\Model;
+use Nette\Utils\Html;
+
+class ContactPresenter extends BasePresenter {
+
+	public function renderAbout() {   
+    	$page = $this->page->findBy(['page' => 'contact'])
+    	                   ->fetch();
+
+        $html = Html::el()->setHtml($page->text);
+        $this->template->text = $html;		
+	}
+}
--- www/js/main.js
+++ www/js/main.js
@@ -3,4 +3,41 @@ $(function(){
       	var href = $(this).children("a").first().attr("href");
       	window.location.href = href;
     });
+
+    function ScaleListTitle() {
+		var container = $('.scale-text');
+		var text = container.find('span');
+		var font_size = parseInt(text.css('font-size'));
+
+		//pokud má kontejner menší šířku než text, sníží se velikost fontu dokud se nevejde
+		while(container.width() < text.width()) {
+			font_size -= 1;
+			text.css('font-size', font_size);
+		}
+		
+		//pokud je kontejner širší než text zvýší se velikost fontu dokud není větší nebo není fontsize 30
+		if(container.width() > text.width()) {
+			while(font_size < 30) {
+				font_size += 1;
+				text.css('font-size', font_size);
+				if(container.width() < text.width()) {
+					text.css('font-size', font_size - 1);
+					break;
+				}
+			}
+		}
+
+		// nakonec korekce font-size
+		font_size = parseInt(text.css('font-size'));
+		if(font_size < 30)
+			text.css('font-size', font_size + 1);
+		if(font_size > 30)
+			text.css('font-size', 30);
+	}
+
+	ScaleListTitle();
+        
+    $(window).bind("load", ScaleListTitle);
+    $(window).bind("resize", ScaleListTitle);
+    $(window).bind("orientationchange", ScaleListTitle);
 });
