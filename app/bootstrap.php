12fd05571c2a7561eaeabdb9322efd6e4f623364
Muktakéša dás (Mac Mini) <muktakesa.bvks@gmail.com>
2017-05-07 09:20:19 +0200

    Link mail

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
 
-$configurator->setDebugMode(TRUE); // enable for your remote IP
+$configurator->setDebugMode(['2a00:1028:83a2:4e0a:c83:c413:7021:5f26']); // enable for your remote IP
 $configurator->enableDebugger(__DIR__ . '/../log');
 
 
