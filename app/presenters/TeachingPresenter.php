eab4da4e876c3a6e11e97a11c9b308697d2fcf34
Muktakéša dás (Mac Mini) <muktakesa.bvks@gmail.com>
2017-05-27 15:15:31 +0200

    Prev button

--- app/presenters/TeachingPresenter.php
+++ app/presenters/TeachingPresenter.php
@@ -9,12 +9,19 @@ use Tracy\Debugger;
 class TeachingPresenter extends BasePresenter {	
 	public function renderCategory($category_id) {
 		$this->template->articles = $this->article->findBy(['category_id' => $category_id])
-												  ->order('created DESC');
+												  ->order('created DESC, id DESC');
 		$this->template->category = $this->articleCategory->get($category_id);
 	}
 
 	public function renderArticle($article_id) {
 		$article = $this->article->get($article_id);
+		$prev = $this->article->findAll()
+							  ->where('created <= ? AND id != ?', $article->created, $article->id)
+							  ->order('created DESC, id DESC');
+		
+		$previsous_article = $prev->count() > 1 ? $prev->fetch() : null;
+		Debugger::fireLog($previsous_article->id);
+		
 		$this->template->article = $article;
 		$this->template->category = $this->articleCategory->get($article->category_id);
 	}
