25675c1a82b5555e2e4885c6bcdc5151e55023ee
harekrisna <misut@iMac-2.local>
2017-06-02 19:47:26 +0200

    articles undo

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -157,7 +157,7 @@ class RouterFactory
 			'presenter' => array(
 				Route::FILTER_TABLE => array(
 					'aktuality' => 'Actuality',
-					'clanky' => 'Teaching',
+					'clanky' => 'Articles',
 					'fotky' => 'Galery',
 					'o-nas' => 'About',
 					'odkazy' => 'Links'
